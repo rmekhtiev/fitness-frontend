1bc9a4a56f76589417b9590cfa0238286fcbab9d
Leonid Meleshin <leon0399@explabs.ru>
2019-09-07 21:37:51 +0300

    feat(Clients): add empty create dialog

---
+++ components/clients/ClientForm.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-form>
+
+  </v-form>
+</template>
+
+<script>
+    export default {
+        name: "ClientForm",
+
+        props: {
+            value: {
+                type: Object,
+                default: {}
+            },
+
+            isEdit: {
+                type: Boolean,
+                default: false,
+            },
+        },
+
+        computed: {
+            defaultForm() {
+                return {
+
+                }
+            }
+        },
+
+        created() {
+            let newVal = { ...this.value };
+
+            _(this.defaultForm).each((item, index) => {
+                if(!this.value[index] || this.value[index] === null) {
+                    newVal[index] = item;
+                }
+            });
+
+            this.$emit('input', newVal)
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
--- pages/clients/index.vue
+++ pages/clients/index.vue
@@ -48,22 +48,43 @@
       </template>
     </v-data-iterator>
 
+    <v-btn
+      color="primary"
+      dark
+      fab
+      fixed
+      bottom
+      right
+      @click="openCreateDialog"
+    >
+      <v-icon>mdi-plus</v-icon>
+    </v-btn>
+
+    <client-dialog ref="createDialog" title="Новый клиент"></client-dialog>
   </div>
 </template>
 
 <script>
     import {mapGetters} from 'vuex';
     import ClientListItem from '../../components/clients/ClientListItem';
+    import ClientDialog from "../../components/clients/ClientDialog";
 
     export default {
         components: {
-            'client-list-item': ClientListItem,
+            ClientDialog,
+            ClientListItem,
         },
 
         computed: {
             ...mapGetters({
                 clients: 'clients/all',
             }),
+        },
+
+        methods: {
+            openCreateDialog() {
+                this.$refs.createDialog.open()
+            }
         },
 
         fetch({store}) {
