90f65757292362434115198f9201cd9b919028c8
Leonid Meleshin <leon0399@explabs.ru>
2019-09-09 15:31:31 +0300

    feat(Halls): update list item layout

--- components/hall/HallListItem.vue
+++ components/hall/HallListItem.vue
@@ -1,46 +1,43 @@
 <template>
-  <v-list-item>
-    <v-layout>
-      <v-flex xs8 md3>
-        <div style="display: flex; width: 100%">
-          <div style="flex: 1 1 0%;" class="text-truncate">
-            <div class="body-2 text-truncate" :title="hall.title">{{ hall.title }}</div>
-            <div class="caption text-truncate" :title="hall.address">{{ hall.address }}</div>
-          </div>
+  <v-layout>
+    <v-flex xs8 sm6>
+      <div style="display: flex; width: 100%">
+        <div style="flex: 1 1 0%;" class="text-truncate">
+          <div class="body-2 text-truncate" :title="hall.title">{{ hall.title }}</div>
+          <div class="caption text-truncate" :title="hall.address">{{ hall.address }}</div>
         </div>
-      </v-flex>
+      </div>
+    </v-flex>
 
-      <v-flex md1>
-        <div style="display: flex; width: 100%">
-          <div style="flex: 1 1 0%;" class="text-truncate">
-            <div class="body-2" >{{ hall.clients_count }}</div>
-            <div class="caption green--text">+ {{ hall.clients_count }}</div>
-          </div>
+    <v-flex xs4 sm2>
+      <div style="display: flex; width: 100%">
+        <div style="flex: 1 1 0%;" class="text-truncate text-right">
+          <div class="body-2">{{ hall.clients_count }}</div>
+          <div class="caption green--text">+ {{ hall.clients_count_new }}</div>
         </div>
-      </v-flex>
+      </div>
+    </v-flex>
 
-      <v-flex md1>
-        <div style="display: flex; width: 100%">
-          <div style="flex: 1 1 0%;" class="text-truncate">
-            <div class="body-2" >{{ hall.lockers_count }} <span class="grey--text">({{ hall.lockers_count_free }})</span></div>
+    <v-flex hidden-xs-only sm2>
+      <div style="display: flex; width: 100%">
+        <div style="flex: 1 1 0%;" class="text-truncate text-right">
+          <div class="body-2">{{ hall.lockers_count }} <span class="grey--text">({{ hall.lockers_count_free }})</span>
           </div>
         </div>
-      </v-flex>
+      </div>
+    </v-flex>
 
-      <v-flex md1>
-        <div style="display: flex; width: 100%">
-          <div style="flex: 1 1 0%;" class="text-truncate">
-            <div class="body-2" >{{ hall.employees_count }}</div>
-          </div>
+    <v-flex hidden-xs-only sm2>
+      <div style="display: flex; width: 100%">
+        <div style="flex: 1 1 0%;" class="text-truncate text-right">
+          <div class="body-2">{{ hall.employees_count }}</div>
         </div>
-      </v-flex>
-    </v-layout>
-  </v-list-item>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
-    import client from "../../mixins/client";
-
     export default {
         name: "hall-list-item",
 
@@ -49,11 +46,7 @@
                 type: Object,
                 required: true
             }
-        },
-
-        mixins: [
-            client,
-        ]
+        }
     }
 </script>
 
