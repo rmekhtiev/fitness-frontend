31e43e7bdba6c283e4470b7cb50b7ee1710c1aa1
Leonid Meleshin <leon0399@explabs.ru>
2019-10-10 02:38:40 +0300

    feat(Bar): add search

--- pages/bar-items/index.vue
+++ pages/bar-items/index.vue
@@ -1,52 +1,84 @@
 <template>
-    <div id="bar-items">
-        <v-data-iterator :items="barItems" :items-per-page="50">
-            <template v-slot:header>
-                <v-layout class="px-4 mt-2 mb-3" style="color: rgba(0, 0, 0, .54);">
-                    <v-flex xs8 md9>
-                        <div style="display: flex; width: 100%">
-                            <div style="flex: 1 1 0%;" class="overline text-truncate">
-                               Название
-                            </div>
-                        </div>
-                    </v-flex>
+  <div id="bar-items">
+    <v-layout id="filters">
+      <v-flex md3 class="hidden-sm-and-down">
+        <v-text-field
+          v-model="filter.title"
 
-                    <v-flex md3>
-                        <div style="display: flex; width: 100%">
-                            <div style="flex: 1 1 0%;" class="overline text-truncate text-right">
-                                Количество
-                            </div>
-                        </div>
-                    </v-flex>
-                </v-layout>
-            </template>
+          prepend-inner-icon="search"
+          label="Поиск"
+          single-line
+          filled
 
-            <template v-slot:default="props">
-                <v-card>
-                    <v-list>
-                        <template v-for="item in props.items">
-                            <bar-item-list-item :barItem="item"></bar-item-list-item>
-                            <v-divider></v-divider>
-                        </template>
-                    </v-list>
-                </v-card>
+          clearable
+
+          @keyup.enter="loadItems"
+        ></v-text-field>
+      </v-flex>
+    </v-layout>
+
+    <v-data-iterator
+      :items="items"
+      :options.sync="iteratorOptions"
+      :server-items-length="totalItems"
+      :loading="itemsLoading"
+
+      :items-per-page="15">
+      <template v-slot:header>
+        <v-layout class="px-4 mt-2 mb-3" style="color: rgba(0, 0, 0, .54);">
+          <v-flex xs8 md9>
+            <div style="display: flex; width: 100%">
+              <div style="flex: 1 1 0%;" class="overline text-truncate">
+                Название
+              </div>
+            </div>
+          </v-flex>
+
+          <v-flex md3>
+            <div style="display: flex; width: 100%">
+              <div style="flex: 1 1 0%;" class="overline text-truncate text-right">
+                Количество
+              </div>
+            </div>
+          </v-flex>
+        </v-layout>
+      </template>
+
+      <template v-slot:default="props">
+        <v-card>
+          <v-list>
+            <template v-for="item in props.items">
+              <bar-item-list-item :barItem="item"></bar-item-list-item>
+              <v-divider></v-divider>
             </template>
-        </v-data-iterator>
-    </div>
+          </v-list>
+        </v-card>
+      </template>
+    </v-data-iterator>
+  </div>
 </template>
 
 <script>
     import BarItemListItem from "../../components/bar-items/BarItemListItem";
+
+    import serverSidePaginated from "../../mixins/server-side-paginated";
+    import selectedHallAware from "../../mixins/selectedHallAware";
+
     export default {
-        components:{
+        components: {
             BarItemListItem,
         },
 
-        computed: {
-            barItems() {
-                return this.$store.getters['bar-items/all'];
-            },
-        },
+        mixins: [
+            selectedHallAware,
+            serverSidePaginated,
+        ],
+
+        data: () => ({
+            resource: 'bar-items',
+        }),
+
+        computed: {},
 
         fetch({store}) {
             return Promise.all([
