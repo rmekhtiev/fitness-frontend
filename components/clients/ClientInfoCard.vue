61c9d130559e370e2975a3a99868261f6b8703a4
Rusiano <rs.mekhtiev@gmail.com>
2019-10-18 18:44:08 +0300

    fix(Clients): fixes instagram display

--- components/clients/ClientInfoCard.vue
+++ components/clients/ClientInfoCard.vue
@@ -68,7 +68,7 @@
           </v-list-item-content>
         </v-list-item>
 
-        <v-list-item v-if="client.phone_number">
+        <v-list-item v-if="client.instagram">
           <v-list-item-icon>
             <v-icon color="primary">
               mdi-instagram
