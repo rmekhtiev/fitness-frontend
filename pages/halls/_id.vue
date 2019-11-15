e04d0664de2983c27132c217163de14e25168f5b
Rusiano <rs.mekhtiev@gmail.com>
2019-11-15 16:30:23 +0300

    fix(stats): fixes loadPayments function and filters

--- pages/halls/_id.vue
+++ pages/halls/_id.vue
@@ -59,14 +59,15 @@
           </v-date-picker>
         </v-dialog>
       </v-flex>
-      <div>{{ bar }}</div>
+      <div>{{ barPayments }}</div>
+      <div>{{ barItems }}</div>
     </v-layout>
     <v-layout wrap row>
       <v-flex xs12>
-<!--        <stats-money-table :items="calculateSum"></stats-money-table>-->
+        <!--        <stats-money-table :items="calculateSum"></stats-money-table>-->
       </v-flex>
       <v-flex xs12>
-        <bar-payments-table :payments="bar"></bar-payments-table>
+        <bar-payments-table :payments="barPayments"></bar-payments-table>
       </v-flex>
     </v-layout>
   </div>
@@ -106,155 +107,75 @@ export default {
   }),
 
   computed: {
-    barPaymentsFilter() {
-      return {
-        sellable_type: "bar-items",
-        ...this.pureFilter
-      };
-    },
-    trainingPaymentsFilter() {
-      return {
-        sellable_type: "trainings",
-        ...this.pureFilter
-      };
-    },
     pureFilter() {
       return _(this.filter)
         .omitBy(_.isNull)
         .omitBy(_.isUndefined)
         .value();
     },
-
-    hall() {
-      return this.$store.getters["halls/byId"]({ id: this.$route.params.id });
+    barItemsFilter() {
+      const items = this.$store.getters["payments/where"]({
+        filter: this.barPaymentsFilter
+      });
+      const barItemsIds = _(items)
+        .map(payment => payment.sellable_id)
+        .uniq()
+        .value();
+      return {
+        bar_item_id: barItemsIds
+      };
     },
-    bar() {
+    barPaymentsFilter() {
+      return {
+        sellable_type: "bar-items",
+        ...this.pureFilter
+      };
+    },
+    barPayments() {
       return this.$store.getters["payments/where"]({
         filter: this.barPaymentsFilter
       });
-    },
-
-    calculateSum() {
-      const payments = this.barPayments;
-      const result = {
-        bar: {
-          cash: 0,
-          card: 0,
-          transfer: 0,
-          total: 0
-        },
-        trainers: {
-          cash: 0,
-          card: 0,
-          transfer: 0,
-          total: 0
-        },
-        total: {
-          cash: 0,
-          card: 0,
-          transfer: 0,
-          total: 0
-        }
-      };
-      for (let i = 0; i < payments.length; i++) {
-        const cost = parseFloat(payments[i].cost * payments[i].quantity);
-        if (payments[i].sellable_type === "bar-items") {
-          if (payments[i].method === "cash") {
-            result.bar.cash += cost;
-          }
-          if (payments[i].method === "card") {
-            result.bar.card += cost;
-          }
-          if (payments[i].method === "transfer") {
-            result.bar.transfer += cost;
-          }
-        }
-
-        if (payments[i].sellable_type === "trainings") {
-          if (payments[i].method === "cash") {
-            result.trainers.cash += cost;
-          }
-          if (payments[i].method === "card") {
-            result.trainers.card += cost;
-          }
-          if (payments[i].method === " transfer ") {
-            result.trainers.transfer += cost;
-          }
-        }
-      }
-      result.bar.total =
-        result.bar.cash + result.bar.card + result.bar.transfer;
-      result.trainers.total =
-        result.trainers.cash + result.trainers.card + result.trainers.transfer;
-      result.total.cash = result.bar.cash + result.trainers.cash;
-      result.total.card = result.bar.card + result.trainers.card;
-      result.total.transfer = result.bar.transfer + result.trainers.transfer;
-      result.total.total = result.bar.total + result.trainers.total;
-      return result;
     }
-  },
-
-  fetch: ({ store, params }) => {
-    return Promise.all([
-      store.dispatch("halls/loadById", {
-        id: params.id
-      })
-    ]);
   },
 
   mounted() {
     this.standartTimeFilter();
-    return Promise.all([this.load()]);
+    return Promise.all([this.loadPayments()]);
   },
 
   methods: {
     loadSubject(payments, type) {
-      const chunks = _(payments).chunk(10);
       switch (type) {
         case "bar-items":
-          return Promise.all(
-            chunks
-              .map(chunk =>
-                Promise.all([
-                  this.$store.dispatch("payments/loadWhere", {
-                    filter: this.barPaymentsFilter
-                  }),
-                  this.$store.store.dispatch("bar-items/loadWhere", {
-                    filter: {
-                      bar_item_id: _(chunk)
-                        .map(payment => payment.sellable_id)
-                        .uniq()
-                        .value()
-                    }
-                  })
-                ])
+          return this.$store
+            .dispatch("payments/loadWhere", {
+              filter: this.barPaymentsFilter
+            })
+            .then(() => {
+              const barItemsIds = _(
+                this.$store.getters["payments/where"]({
+                  filter: this.barPaymentsFilter
+                })
               )
-              .value()
-          );
+                .map(payment => payment.sellable_id)
+                .uniq().value();
+
+              console.info("Gonna load next sellable ids: " + barItemsIds);
+
+              return Promise.all([
+                this.$store.dispatch("bar-items/loadWhere", {
+                  filter: {
+                    bar_item_id: barItemsIds
+                  }
+                })
+              ]);
+            });
         default:
-          return Promise.all(
-            chunks
-              .map(chunk =>
-                Promise.all([
-                  this.$store.dispatch("payments/loadWhere", {
-                    filter: this.pureFilter
-                  }),
-                  this.$store.dispatch("bar-items/loadWhere", {
-                    filter: {
-                      bar_item_id: _(chunk)
-                        .map(payment => payment.sellable_id)
-                        .uniq()
-                        .value()
-                    }
-                  })
-                ])
-              )
-              .value()
-          );
+          return 0;
       }
     },
 
-    load() {
+    loadPayments() {
       this.$store
         .dispatch("payments/loadPage", {
           options: {
@@ -275,33 +196,12 @@ export default {
     },
     saveStartDateFilter() {
       this.$refs.startDialog.save(this.filter.start);
-      this.load();
+      this.loadPayments();
     },
     saveEndDateFilter() {
       this.$refs.endDialog.save(this.filter.end);
-      this.load();
+      this.loadPayments();
     },
-    // loadBarItems() {
-    //   const items = this.$store.getters["payments/where"]({
-    //     filter: this.barPaymentsFilter
-    //   });
-    //   const barItemsIds = _(items)
-    //     .map(barItem => barItem.sellable_id)
-    //     .value();
-    //   return this.$store.dispatch("bar-items/loadWhere", {
-    //     filter: { bar_item_id: barItemsIds }
-    //   });
-    // },
-    // loadBarPayments() {
-    //   this.$store.dispatch(this.resource + "/loadWhere", {
-    //     filter: this.barPaymentsFilter
-    //   });
-    // },
-    // loadTrainingPayments() {
-    //   this.$store.dispatch(this.resource + "/loadWhere", {
-    //     filter: this.trainingPaymentsFilter
-    //   });
-    // },
     standartTimeFilter() {
       this.filter.start = this.$moment().format("YYYY-MM-DD");
       this.filter.end = this.$moment().format("YYYY-MM-DD");
